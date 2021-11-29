6c4c4014c979719c37cdf2498bac7ed7a622893c
Nam <62420404+HoangNamDo@users.noreply.github.com>
2021-11-29 07:06:17 +0000

    h

--- main.py
+++ main.py
@@ -8,39 +8,46 @@ import pandas as pd
 def display_menu():
     print()
     print("* * * * * * * COMMAND MENU * * * * * * *")
-    print("1 - Print Name, Type, Generation")
-    print("2 - Print Name, HP, Attack, Defense, Speed")
-    print("3 - Print all of the GRASS type Pokemon")
-    print("4 - Print all of the Pokemon in order of HP (highest to lowest)")
-    print("5 - Print all of the Pokemon in order of NAME A-Z")
+    print("1 - Display Pokemons by Name, Type, Generation")
+    print("2 - Display Name, HP, Attack, Defense, Speed")
+    print("3 - Display all of the GRASS type Pokemon")
+    print("4 - Display all of the Pokemon in order of HP (highest to lowest)")
+    print("5 - Display all of the Pokemon in order of NAME A-Z")
     print("6 - all the LEGENDARY Pokemon")
     print("7 - all the LEGENDARY Pokemon")
 
-def display_name_type_and_generation(pokemon):
+def display_by_name_type_and_generation(pokemon):
     print(pokemon[["Name", "Type 1", "Type 2", "Generation"]])
 
-def display_name_hp_attack_defense_speed(pokemon):
+def display_by_name_hp_attack_defense_and_speed(pokemon):
     print(pokemon[["Name", "HP", "Attack", "Defense", "Speed"]])
 
-def display_dataframe_grass_type_pokemon(pokemon):
+def display_dataframe_of_only_grass_type(pokemon):
+    # this function displays only GRASS type, yet no matter Type 1 GRASS or Type 2 GRASS
     grass_type_pokemon = pokemon[(pokemon["Type 1"] == "Grass")|(pokemon["Type 2"] == "Grass")]
     frame_grass_type_pokemon = pd.DataFrame(grass_type_pokemon)
     print(frame_grass_type_pokemon[["Name", "Type 1", "Type 2"]])
 
-def display_dataframe_pokemon_hp_highest_to_lowest(pokemon):
+def display_dataframe_of_highest_to_lowest_hp(pokemon):
     pokemon_hp_highest_to_lowest = pokemon.sort_values("HP", ascending=False)
     frame_pokemon_hp_highest_to_lowest = pd.DataFrame(pokemon_hp_highest_to_lowest)
     print(frame_pokemon_hp_highest_to_lowest[["Name", "HP"]])
 
-def display_dataframe_pokemon_name_in_ascending_order(pokemon):
+def display_dataframe_of_name_in_ascending_order(pokemon):
     pokemon_name_in_ascending_order = pokemon.sort_values("Name")
     frame_pokemon_name_in_ascending_order = pd.DataFrame(pokemon_name_in_ascending_order)
-    print(frame_pokemon_name_in_ascending_order["Name"])
+    print(frame_pokemon_name_in_ascending_order)
 
-def display_dataframe_legendary_pokemon(pokemon):
+def display_dataframe_of_only_legendary(pokemon):
     legendary_pokemon = pokemon[pokemon["Legendary"] == True]
     frame_legendary_pokemon = pd.DataFrame(legendary_pokemon)
     print(frame_legendary_pokemon[["Name", "Legendary"]])
+
+def search_by_name_and_display(pokemon):
+    search_value = input("Input a name of a Pokemon: ")
+    associated_data = pokemon[pokemon["Name"] == search_value]
+    frame_associated_data = pd.DataFrame(associated_data)
+    print(frame_associated_data)
 
 def main():
     # print("Welcome to MCU Superheroes Database")
@@ -55,15 +62,18 @@ def main():
         if command == "1":
             display_name_type_and_generation(pokemon)
         elif command == "2":
-            display_name_hp_attack_defense_speed(pokemon)
+            display_name_hp_attack_defense_and_speed(pokemon)
         elif command == "3":
-            display_dataframe_grass_type_pokemon(pokemon)
+            # display only GRASS type, yet no matter Type 1 GRASS or Type 2 GRASS
+            display_dataframe_only_grass_type(pokemon)
         elif command == "4":
-            display_dataframe_pokemon_hp_highest_to_lowest(pokemon)
+            display_dataframe_hp_highest_to_lowest(pokemon)
         elif command == "5":
-            display_dataframe_pokemon_name_in_ascending_order(pokemon)
-        # elif command == "6":
-        #     break
+            display_dataframe_name_in_ascending_order(pokemon)
+        elif command == "6":
+            display_dataframe_only_legendary(pokemon)
+        elif command == "7":
+            break
         else:
             print("Unknown command. Please try again.")
 
