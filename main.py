76ced1e53fd0a7a518da7c5d09185ad11e15a825
Nam <62420404+HoangNamDo@users.noreply.github.com>
2021-11-29 06:33:53 +0000

    3

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-# ISIT 333 Lab 9
+# ISIT 333 - Lab 9
 # November 28, 2021
 # Hoang Do
 
-# first import pandas
+# import pandas
 import pandas as pd
-
-# ASSIGNMENT INSTRUCTIONS ---
-
-# BUILD A MENU SYSTEM WITH OPTIONS FOR THE FOLLOWING OPTIONS
-
-# 1 - Print out a report with only the following fields displaying - 
-
-# 2 - Print out a report displaying the  
-
-# 3 - Create a dataFrame displaying all of the GRASS type Pokemon in the csv file
-
-# 4 - Create a dataFrame displaying all of the Pokemon in order of HP (highest to lowest)
-
-# 5 - Create a dataFrame displaying all of the Pokemon in order of NAME A-Z
-
-# 6 - Create a dataFrame of all the LEGENDARY Pokemon. 
-
-# 7 - Create a search for a name of a Pokemon and return the data associated with the Pokemon. Do a try/except to catch any error or record not found. Allow the user to search multiple times until they choose to exit.
 
 def display_menu():
     print()
@@ -34,68 +16,58 @@ def display_menu():
     print("6 - all the LEGENDARY Pokemon")
     print("7 - all the LEGENDARY Pokemon")
 
-def display_name_type_generation(pokemon):
+def display_name_type_and_generation(pokemon):
     print(pokemon[["Name", "Type 1", "Type 2", "Generation"]])
 
 def display_name_hp_attack_defense_speed(pokemon):
     print(pokemon[["Name", "HP", "Attack", "Defense", "Speed"]])
 
-def display_grass_type_pokemon_dataframe(pokemon):
-    # fram = 
-    # print(fram)
+def display_dataframe_grass_type_pokemon(pokemon):
     grass_type_pokemon = pokemon[pokemon["Type 1"] == "Grass"]
-    grass_type_pokemon_frame = pd.DataFrame(grass_type_pokemon)
+    frame_grass_type_pokemon = pd.DataFrame(grass_type_pokemon)
     # print(pokemon[["Name", "HP", "Attack", "Defense", "Speed"]])
-    print(grass_type_pokemon_frame)
+    print(frame_grass_type_pokemon[["Name", "Type 1", "Type 2"]])
+
+def display_dataframe_pokemon_hp_highest_to_lowest(pokemon):
+    pokemon_hp_highest_to_lowest = pokemon.sort_values("HP", ascending=False)
+    frame_pokemon_hp_highest_to_lowest = pd.DataFrame(pokemon_hp_highest_to_lowest)
+    print(frame_pokemon_hp_highest_to_lowest[["Name", "HP"]])
+
+def display_dataframe_pokemon_name_in_ascending_order(pokemon):
+    pokemon_name_in_ascending_order = pokemon.sort_values("Name")
+    frame_pokemon_name_in_ascending_order = pd.DataFrame(pokemon_name_in_ascending_order)
+    print(frame_pokemon_name_in_ascending_order["Name"])
+
+def display_dataframe_legendary_pokemon(pokemon):
+    legendary_pokemon = pokemon[pokemon["Legendary"] == True]
+    frame_legendary_pokemon = pd.DataFrame(legendary_pokemon)
+    print(frame_legendary_pokemon[["Name", "Legendary"]])
 
 def main():
     # print("Welcome to MCU Superheroes Database")
-    # then save data to a variable "pokemon"
+    # save data to a variable "pokemon"
     pokemon = pd.read_csv("pokemon.csv")
 
-    ## EXAMPLE DATA CALLS - COMMENT OR REMOVE lines 7 - 25 BEFORE SUBMITTING
-
-    # # print first 5 records
-    # print()
-    # print("Top 5 records\n")
-    # print(pokemon.head(5))
-
-    # # print information about csv data
-    # print()
-    # print("Basic information about the Pokemon data\n")
-    # print(pokemon.info())
-
-    # # print statistical information about all records in the data file
-    # print()
-    # print("Basic statistical information about the Pokemon data\n")
-    # print(pokemon.describe())
-
-    # # print the minimum value of HP column - note this should align with the data in the describe() function
-    # print("The minimum HP value is", pokemon["HP"].min())
     while True:
         display_menu()
         print()
         command = input("Command: ")
         print()
         if command == "1":
-            display_name_type_generation(pokemon)
+            display_name_type_and_generation(pokemon)
         elif command == "2":
             display_name_hp_attack_defense_speed(pokemon)
         elif command == "3":
-            display_grass_type_pokemon_dataframe(pokemon)
-        # elif command == "4":
-        #     list_portrayed_by(conn, cursor)
-        # elif command == "5":
-        #     cursor = add_new_superhero(conn, cursor)
+            display_dataframe_grass_type_pokemon(pokemon)
+        elif command == "4":
+            display_dataframe_pokemon_hp_highest_to_lowest(pokemon)
+        elif command == "5":
+            display_dataframe_pokemon_name_in_ascending_order(pokemon)
         # elif command == "6":
         #     break
         else:
             print("Unknown command. Please try again.")
 
-    # save the updates/inserts to the database
-    conn.commit()
-    # close the connection
-    conn.close()
     # farewell message
     print("Thank you for using MCU superhero database. See you later for more interesting about Marvel Cinematic Universe!")
 
