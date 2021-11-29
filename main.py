bfbc30fd172bdcaa04bd5070a7d29c1232d60525
Nam <62420404+HoangNamDo@users.noreply.github.com>
2021-11-29 07:24:11 +0000

    3

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ def display_menu():
     print("* * * * * * * COMMAND MENU * * * * * * *")
     print("1 - Display all of the Pokemon by Name, Type, Generation")
     print("2 - Display all of the Pokemon by Name, HP, Attack, Defense, Speed")
-    print("3 - Display all of the GRASS type Pokemon")
+    print("3 - Display only the GRASS type Pokemon")
     print("4 - Display all of the Pokemon in order of HP (highest to lowest)")
     print("5 - Display all of the Pokemon in order of NAME A-Z")
-    print("6 - Display all of the LEGENDARY Pokemon")
+    print("6 - Display only the LEGENDARY Pokemon")
     print("7 - Search the Pokemon by Name and Display the Associated Data")
+    print("8 - Exit the Program")
 
 def display_by_name_type_and_generation(pokemon):
     print(pokemon[["Name", "Type 1", "Type 2", "Generation"]])
@@ -23,7 +24,7 @@ def display_by_name_hp_attack_defense_and_speed(pokemon):
     print(pokemon[["Name", "HP", "Attack", "Defense", "Speed"]])
 
 def display_dataframe_of_only_grass_type(pokemon):
-    # this function displays only GRASS type, yet no matter Type 1 GRASS or Type 2 GRASS
+    # this function displays only GRASS type, no matter Type 1 GRASS or Type 2 GRASS
     grass_type_pokemon = pokemon[(pokemon["Type 1"] == "Grass")|(pokemon["Type 2"] == "Grass")]
     frame_grass_type_pokemon = pd.DataFrame(grass_type_pokemon)
     print(frame_grass_type_pokemon[["Name", "Type 1", "Type 2"]])
@@ -44,13 +45,17 @@ def display_dataframe_of_only_legendary(pokemon):
     print(frame_legendary_pokemon[["Name", "Legendary"]])
 
 def search_by_name_and_display(pokemon):
-    search_value = input("Input a name of a Pokemon: ")
-    associated_data = pokemon[pokemon["Name"] == search_value]
-    frame_associated_data = pd.DataFrame(associated_data)
-    print(frame_associated_data)
+    try:
+        search_value = input("Input a name of a Pokemon: ")
+        associated_data = pokemon[pokemon["Name"] == search_value]
+        frame_associated_data = pd.DataFrame(associated_data)
+        print(frame_associated_data)
+    except:
+        print("Something went wrong. Please try again!")
+        search_value = input("Input a name of a Pokemon: ")
 # Name,Type 1,Type 2,Total,HP,Attack,Defense,Sp. Atk,Sp. Def,Speed,Generation,Legendary
 def main():
-    # print("Welcome to MCU Superheroes Database")
+    print("Welcome to the Incredible Pokemon Data")
     # save data to a variable "pokemon"
     pokemon = pd.read_csv("pokemon.csv")
 
@@ -64,7 +69,7 @@ def main():
         elif command == "2":
             display_by_name_hp_attack_defense_and_speed(pokemon)
         elif command == "3":
-            # display only GRASS type, yet no matter Type 1 GRASS or Type 2 GRASS
+            # display only GRASS type, no matter Type 1 GRASS or Type 2 GRASS
             display_dataframe_of_only_grass_type(pokemon)
         elif command == "4":
             display_dataframe_of_highest_to_lowest_hp(pokemon)
@@ -73,12 +78,14 @@ def main():
         elif command == "6":
             display_dataframe_of_only_legendary(pokemon)
         elif command == "7":
+            search_by_name_and_display(pokemon)
+        elif command == "8":
             break
         else:
             print("Unknown command. Please try again.")
 
     # farewell message
-    print("Thank you for using MCU superhero database. See you later for more interesting about Marvel Cinematic Universe!")
+    print("Thank you for using the Incredible Pokemon Data. See you later for more interesting about the Pokemon!")
 
 if __name__ == "__main__":
     main()
